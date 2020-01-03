5cbb214cd0ddae63f994ed6cfe905afdb148f6f3
Kosztolanyi <Kosztolanyi.baji.zsolt@gmail.com>
2020-01-03 13:57:44 +0100

    variables

---
+++ java_basics/src/java_basics/Variables.java
@@ -0,0 +1,29 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int x;
+		x = 34;
+		
+		System.out.println(x);
+		
+		x = 64;
+
+		System.out.println(x);
+		
+		String words;
+		
+		words = "makika";
+		
+		System.out.println(words);
+		
+		String newWord = "hapci";
+		
+		System.out.println(newWord);
+		
+		System.out.println(x + 6);
+
+	}
+
+}
